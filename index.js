898dd376839a19eaec5c2f7a853640a3c8b52d7b
Emine Çığ <emine.cig67@gmail.com>
2022-04-26 14:46:20 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+function saturdayFun( funType ="roller-skate"){
+    return `This Saturday, I want to ${funType}!` 
+}
+function mondayWork(workType ="go to the office"){
+    return`This Monday, I will ${workType}.`
+}
+
+function wrapAdjective(arg="*"){
+    return function(type ="special"){
+        return `You are ${arg}${type}${arg}!`
+    }
+}
